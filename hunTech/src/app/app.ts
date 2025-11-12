25ee37ff3fec592db5932d03bb83bc2d3152cb24
jlarata <ariel@cfee.gob.ar>
2025-11-12 09:21:48 -0300

    modificada la lógica de la carga de datos de usuario en el ts y el html

--- hunTech/src/app/app.ts
+++ hunTech/src/app/app.ts
@@ -1,18 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 
 import { Navbar } from './componentes/navbar/navbar';
 import { Users } from './servicios/users';
 import { Auth } from './servicios/auth';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     Navbar,
-    RouterOutlet, 
-    RouterModule, 
+    RouterOutlet,
+    RouterModule,
     CommonModule
   ],
   templateUrl: './app.html',
@@ -21,15 +22,48 @@ import { Auth } from './servicios/auth';
 export class App {
 
   title = 'hunTech';
-
   authService = inject(Auth);
-
   usersService = inject(Users);
-  
+
+  isAuth?: boolean | undefined;
+  existUser?: boolean | undefined;
+  isDataLoaded: boolean = false;
+
+  ngOnInit(): void {
+    this.isDataLoaded = false;
+    this.loadData();
+  }
+
+
   onRoleChange(event: Event): void {
     const select = event.target as HTMLSelectElement;
     const value = select.value;
     this.usersService.setSelectedRole(value);
+  }
+
+  private loadData(): void {
+    
+
+    this.authService.isAuthenticated$.subscribe({
+      next: (data) => {
+        this.isAuth = data
+      },
+      error: (err) => console.error('Error al obtener usuario', err)
+    })
+
+    this.usersService.isExistUser$
+    .pipe(
+      tap((data) => { if (data) {
+        this.isDataLoaded = true
+      } }
+    )
+    )
+    .subscribe({
+      next: (data) => {
+        this.existUser = data
+      },
+      error: (err) => console.error('Error al obtener usuario', err)
+    })
   }
 }
 
