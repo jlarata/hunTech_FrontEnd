363005d036c389d6df6e030ae1cc638c384550d5
monim <monibrave1@gmail.com>
2026-04-26 20:42:02 -0300

    aplico guard a las rutas que no deberian cargarse sin auth

--- hunTech/src/app/app.routes.ts
+++ hunTech/src/app/app.routes.ts
@@ -1,43 +1,57 @@
 import { Routes } from '@angular/router';
 import { Home } from './componentes/home/home';
+import { authGuard } from './servicios/guards/authGuard';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', loadComponent: () => import('./componentes/home/home').then((m) => m.Home) },
   {
     path: 'contratos',
+    canActivate: [authGuard],
     loadComponent: () => import('./componentes/contratos/contratos').then((m) => m.Contratos),
   },
   /* { path: 'contrato-detail', loadComponent: () => import('./componentes/contrato-detail/contrato-detail').then(m => m.ContratoDetail) }, */
   {
     path: 'profile',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./componentes/profile/profile.component').then((m) => m.ProfileComponent),
   },
   {
     path: 'miproyecto',
+    canActivate: [authGuard],
     loadComponent: () => import('./componentes/miproyecto/miproyecto').then((m) => m.Miproyecto),
   },
   {
     path: 'formcreateproyect',
+    canActivate: [authGuard],
     loadComponent: () => import('./views/formcreateproyect/formcreateproyect').then((m) => m.Formcreateproyect)
   },
   {
     path: 'formcreateproyect/:email',
+    canActivate: [authGuard],
     loadComponent: () => import('./views/formcreateproyect/formcreateproyect').then((m) => m.Formcreateproyect)
   },
   {
     path: 'formcreatecontract',
+    canActivate: [authGuard],
     loadComponent: () => import('./views/formcreatecontract/formcreatecontract').then((m) => m.Formcreatecontract)
   },
   {
     path: 'formcreatecontract/:project_id',
+    canActivate: [authGuard],
     loadComponent: () => import('./views/formcreatecontract/formcreatecontract').then((m) => m.Formcreatecontract)
   },
-  { path: 'profile/:email',loadComponent: () =>
+  { 
+    path: 'profile/:email',
+    canActivate: [authGuard],
+    loadComponent: () =>
       import('./componentes/profile/profile.component').then((m) => m.ProfileComponent)
   },
-  { path: 'dashboard', loadComponent: () =>
+  { 
+    path: 'dashboard',
+    canActivate: [authGuard], 
+    loadComponent: () =>
     import('./componentes/dashboard/dashboard').then((m) => m.Dashboard)
   },
 
