3fd1230930e407e0309cc5a4801c38013e1d5403
Matias Campopiano <maticampopiano@hotmail.com>
2026-05-03 00:09:20 -0300

    costato que por defecto llame a vercel

--- hunTech/src/app/environments/environment.ts
+++ hunTech/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://backend-huntech.vercel.app/api/',
   supabaseUrl: 'https://qsjliwlbulaheauemozc.supabase.co',
   supabaseKey: 'sb_publishable_zJh8D56soAZWHlNO3fnD4A_kfOlO4CX',
   gests: [] as string[],
